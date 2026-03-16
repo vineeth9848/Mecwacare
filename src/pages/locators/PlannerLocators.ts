58c31ea5cb5db26efc28efe4f9ea3f9ffdf336a1
vineeth9848 <2474707@cognizant.com>
2026-03-16 21:33:34 +0530

    added send for signature and closed won opportunity flows

---
+++ src/pages/locators/PlannerLocators.ts
@@ -0,0 +1,9 @@
+export const PlannerLocators = {
+  newButton: "button[name='New'], a[title='New'], button:has-text('New')",
+  listViewDropdown: "button[title='Select a List View'], button:has-text('Select a List View')",
+  listViewOption: "[role='listbox'] [role='option'], [role='listbox'] li",
+  listSearchInput: "input[placeholder='Search this list...'], input[aria-label='Search this list...']",
+  tableRows: 'table tbody tr',
+  rowLink: 'th a, td a',
+  detailsTab: "a[title='Details'], [role='tab']:has-text('Details')",
+} as const;
