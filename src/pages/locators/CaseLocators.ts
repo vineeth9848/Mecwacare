5a5bdd35996b12b8d3c62ee89dd3d97040e79d69
Reddy <2474707@cognizant.com>
2026-04-02 19:29:34 +0530

    addressed few script failures in cases

--- src/pages/locators/CaseLocators.ts
+++ src/pages/locators/CaseLocators.ts
@@ -1,10 +1,12 @@
 export const CaseLocators = {
   accountSearchInput: "input[placeholder='Search Accounts...']",
+  invoiceSearchInput: "input[placeholder='Search Invoices...']",
   participantSearchInput: "input[placeholder='Search Contacts...']",
   listboxOptions: "[role='listbox'] [role='option'], [role='listbox'] li",
   searchIconInCombobox: "button[title='Search'], lightning-button-icon button, [data-key='search']",
-  advancedSearchHeading: "h2:has-text('Advanced Search')",
+  advancedSearchHeading: "h1:has-text('Advanced Search')",
   advancedSearchAccountInput: "input[placeholder='Search Accounts...']",
-  advancedSearchFirstRowRadio: "label.slds-radio__label, span.slds-radio_faux, input[type='radio']",
+  advancedSearchFirstRowRadio: "label[class='slds-radio__label']",
   advancedSearchSelectButton: "lightning-modal-footer button.slds-button_brand:has-text('Select'), button.slds-button_brand:has-text('Select')",
+  subjectInput: "input[name='Subject']",
 } as const;
