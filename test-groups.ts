59c7a4585e58f285af43ca29e6fcf7cd0c365fea
Paduri Vineeth Reddy <padurivineeth.reddy@mecwacare.org.au>
2026-04-14 03:26:25 +1000

    Updated report

--- test-groups.ts
+++ test-groups.ts
@@ -2,6 +2,8 @@ export const serialTests = [
   'tests/ui/lead-create.spec.ts',
   'tests/ui/lead-convert.spec.ts',
   'tests/ui/account.spec.ts',
+  'tests/ui/opportunity.spec.ts',
+  'tests/ui/case.spec.ts'
   
   
 ];
